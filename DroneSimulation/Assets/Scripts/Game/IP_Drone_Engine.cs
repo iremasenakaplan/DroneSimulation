eebcbc4e994fa4877823d4954d06d2348bb537bc
İrem Asena Kaplan <81148564+iremasenakaplan@users.noreply.github.com>
2024-10-06 11:15:41 +0300

    up mechanic update

--- DroneSimulation/Assets/Scripts/Game/IP_Drone_Engine.cs
+++ DroneSimulation/Assets/Scripts/Game/IP_Drone_Engine.cs
@@ -9,6 +9,10 @@ namespace DroneCore
     {
         [Header("Engine Properties")]
         [SerializeField] private float maxPower = 4f;
+
+        [Header("Propeller Properties")]
+        [SerializeField] private Transform propeller;
+        [SerializeField] private float propRotSpeed = 300f;
         public void InitEngine()
         {
             throw new System.NotImplementedException();
@@ -17,11 +21,28 @@ namespace DroneCore
         public void UpdateEngine(Rigidbody rb, IP_Drone_Inputs input)
         {
             //Debug.Log("Running Engine: " + gameObject.name);
+            Vector3 upVec = transform.up;
+            upVec.x = 0f;
+            upVec.z = 0f;
+            float diff = 1 - upVec.magnitude;
+            float finalDiff = Physics.gravity.magnitude * diff;
 
             Vector3 engineForce = Vector3.zero;
-            engineForce = transform.up * ((rb.mass * Physics.gravity.magnitude) + (input.Throttle * maxPower)) / 4f;
+            engineForce = transform.up * ((rb.mass * Physics.gravity.magnitude + finalDiff) + (input.Throttle * maxPower)) / 4f;
 
             rb.AddForce(engineForce, ForceMode.Force);
+
+            HandlePropellers();
+        }
+
+        void HandlePropellers()
+        {
+            if(!propeller)
+            {
+                return;
+            }
+
+            propeller.Rotate(Vector3.up, propRotSpeed);
         }
     }
 
